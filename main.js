e2a1d36ee81e6b3de360c2867e7a72e791dcfc7b
Belal Hesham <126694241+BelalAboSeda@users.noreply.github.com>
2023-10-29 14:07:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgslider(anything){
+    document.querySelector('.starbucks').scr = anything
+  }
+  function changecirclecolor(color){
+    const circle = document.querySelector('circle');
+    circle.style.background = color;
+  }
